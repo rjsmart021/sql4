7879e01216bc807aae88bbfecf44aca188c69f08
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-09-21 22:07:10 -0500

    Update .py

--- .py
+++ .py
@@ -31,7 +31,11 @@ def add_workout_session(member_id, date, duration_minutes, calories_burned):
                     VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist())
     conn.commit()
 
-   
+def add_member(member_id,First_Name, Last_Name,age, Gender):
+    cur.execute("""INSERT INTO member_id,First_Name, Last_Name,age, Gender)
+                    VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist())
+    conn.commit()
+    
 def update_member_age(member_id, new_age):
     cur.execute( """UPDATE WorkoutSessions SET age = 'new_age' WHERE member_id = '234';""")
     conn.commit()
@@ -50,8 +54,8 @@ def delete_workout_session(session_id):
 
 
 #Test 1
-add_workout_session(Member_id,10/20/2024,70min,130)
-update_member_age(Member_id,67)
-delete_workout_session(session_id)
+add_member(Sz678, Fitzgera, Kangory, 23, Male)
+add_workout_session(Sz678,10/20/2024,70min,130)
+update_member_age(Sz678,67)
+delete_workout_session(554567)
 get_members_in_age_range((45, 55)
-DB_NAME_DEFAULT,DB_USER,DB_PASSWORD

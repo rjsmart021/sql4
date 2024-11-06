dd3a02258ec8e4b7be9b6cc7774dcc9b3de88c9f
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-05 21:41:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,14 @@ class Workouts(db.Model):
         self.workout_time = workout_time
         self.workout_rating = workout_rating
         self.members_inworkout = []
-   
+from flask_marshmallow import Marshmallow
+from marshmallow import fields, validate
+from API_MINI import app
+ma = Marshmallow()
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+# schema.py
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from app.models import Bakery as BakeryModel, Product as productModel, product_genres, db
+from sqlalchemy.orm import Session

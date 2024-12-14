9e153a0f097e95dd5b44182d15b0fd19426be68d
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-12-14 10:12:14 -0600

    Update .py

--- .py
+++ .py
@@ -1,8 +1,5 @@
-
-import configparser
-import pandas as pd
-from sql_queries import*
 import mysql.connector
+from mysql.connector import Error
 DB_NAME_DEFAULT = "SQL_1"
 DB_USER = "root"
 DB_PASSWORD = "HHappiness0621#"
@@ -31,9 +28,9 @@ def connect_database():
         # Handling any connection errors
         print(f"Error: {e}")
         return None
-Create a database in your MySQL Workbench and fill the db_name, user and password with yours, then use that function to create a connection like:
+# Create a database in your MySQL Workbench and fill the db_name, user and password with yours, then use that function to create a connection like:"
 conn = connect_database()
-And use it inside the functions to perform the different operations, for example, this is a function to inser a member:
+# And use it inside the functions to perform the different operations, for example, this is a function to inser a member:
 def add_member(id, name, age):
     """
     Adds a new member to the 'Members' table.
@@ -73,35 +70,40 @@ conn.commit()
 #Close connction
 conn.close()
 def add_workout_session(member_id, date, duration_minutes, calories_burned):
-    cur.execute("""INSERT INTO WorkoutSessions member_id, date, duration_minutes, calories_burned)
-                    VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist())
+    cur.execute("INSERT INTO WorkoutSessions member_id, date, duration_minutes, calories_burned) VALUES(%s,%s,%s,%s)", 
+                (member_id, date, duration_minutes, calories_burned)
+                )
     conn.commit()
 
 def add_member(member_id,First_Name, Last_Name,age, Gender):
-    cur.execute("""INSERT INTO member_id,First_Name, Last_Name,age, Gender)
-                    VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist())
+    cur.execute("INSERT INTO member_id,First_Name, Last_Name,age, Gender) VALUES(%s,%s,%s,%s,%s)", 
+                (member_id,First_Name, Last_Name,age, Gender)
+                )
     conn.commit()
     
 def update_member_age(member_id, new_age):
-    cur.execute( """UPDATE WorkoutSessions SET age = 'new_age' WHERE member_id = '234';""")
-                    VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist())
+    cur.execute( "UPDATE WorkoutSessions SET age = %s WHERE member_id = %s;)", 
+                    (member_id, new_age)
+                    )
     conn.commit()
 
  # Example code structure
 def delete_workout_session(session_id):
-    cur.execute("""DELETE FROM WorkoutSessions WHERE name = 'session_id'""")
-                VALUES(%s,%s,%s,%s,%s,%d)""", row.tolist()
+    cur.execute("DELETE FROM WorkoutSessions WHERE name = %s)"
+                , (session_id,)
+                )
     conn.commit()
     
 
 def get_members_in_age_range(start_age, end_age):
-    cur.execute("""SELECT * FROM Members WHERE Members BETWEEN 'start_age' AND 'end_age''""")
-                VALUES(%s,%s,%s,%s,%s,%d), row.tolist()
+    cur.execute("SELECT * FROM Members WHERE Members BETWEEN %s AND %s)",
+                (start_age, end_age)
+                )
     conn.commit()
 
 #Test 1
-add_member(Sz678, Fitzgera, Kangory, 24, Male)
-add_workout_session(Sz678,10/20/2024,70,130)
-update_member_age(Sz678,67)
+add_member('Sz678','Fitzgera', 'Kangory', 24, 'Male')
+add_workout_session('Sz678',10/20/2024,70,130)
+update_member_age('Sz678',67)
 delete_workout_session(554567)
 get_members_in_age_range((45, 55))
